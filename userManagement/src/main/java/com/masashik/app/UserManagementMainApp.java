246728cabe06d04fa7f164da070c269abe7298a6
Masashi Kobayashi <masashi@masashi.ca>
2021-08-06 14:12:21 -0400

    This is a test commit to intentionally break the CI build with unchecked bug as proof.

--- userManagement/src/main/java/com/masashik/app/UserManagementMainApp.java
+++ userManagement/src/main/java/com/masashik/app/UserManagementMainApp.java
@@ -12,6 +12,8 @@ public class UserManagementMainApp {
 
 	public static void main(String[] args) {
 
+		Adding some weird text here to test CI server catch it and fail to build.
+
 		/**
 		 * 	This API App has the following functionalities.
 		 *
